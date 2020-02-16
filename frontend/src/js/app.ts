2b85c1d9bdc37a147b954214077fbe78f9b654ba
Przemyslaw Wrzeszcz <przemyslaw.wrzeszcz@gmail.com>
2020-02-16 11:37:34 +0100

    working adding products to the cart

--- frontend/src/js/app.ts
+++ frontend/src/js/app.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosResponse } from 'axios';
 import $ from 'cash-dom';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import { ProductI } from './types/models';
@@ -12,12 +13,29 @@ $(function() {
         Swal.fire({
             title: data.name,
             text: data.description,
+            footer: `Price: ${data.price.toFixed(2)}&#36;`,
             imageUrl: `/dist/images/${data.photo}`,
             imageAlt: `${data.name} photo`,
             confirmButtonText: 'Add to cart',
             showCancelButton: true,
             showCloseButton: true,
             reverseButtons: true,
+            showLoaderOnConfirm: true,
+            allowOutsideClick: (): boolean => !Swal.isLoading(),
+            preConfirm: () => {
+                return axios
+                    .post('/cart/add', {
+                        productId: data.id,
+                    })
+                    .then((response: AxiosResponse): void => {
+                        if (response.status !== 200) {
+                            throw new Error(response.statusText);
+                        }
+                    })
+                    .catch((): void => {
+                        Swal.showValidationMessage('Server error occurred. Please try again in a few minutes');
+                    });
+            },
         }).then((result): void => {
             if (result.value) {
                 Swal.fire({
